17c4483000cb681cd01bebfc5c12fa7b21a37695
illiagitdev <illya.posv.b@gmail.com>
2019-09-18 01:43:45 +0300

    Shaker Sorting

---
+++ ShakerSorting/src/ShakerSorting.java
@@ -0,0 +1,58 @@
+public class ShakerSorting {
+    public static void main(String[] args) {
+
+        /*        new array for shaker sorting      */
+        int array[] = fillArray(20);
+        showArray(array);
+
+        shakerSorting(array);
+
+        showArray(array);
+    }
+
+    /**
+     * Shaker sorting
+     */
+    private static int[] shakerSorting(int[] array) {
+        int tmp;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i; j < array.length - (i + 1); j++) {
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+
+            for (int j = array.length - (i + 2); j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                    tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+
+    /**
+     * Prints array of integers
+     */
+    private static void showArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Generates array of integers with defined size
+     */
+    private static int[] fillArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 200 - 100);
+        }
+        return array;
+    }
+}
