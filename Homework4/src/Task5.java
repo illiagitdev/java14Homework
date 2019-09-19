e75ca6767c38771daf57ed9ecb67256b8f1b33b0
illiagitdev <illya.posv.b@gmail.com>
2019-09-19 17:58:53 +0300

    Homework4: tasks1-4 finished, work on 5**

--- Homework4/src/Task5.java
+++ Homework4/src/Task5.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
+        System.out.print("Введіть число: ");
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
 
+        printValue(value);
+    }
+
+    //  first variation
+    private static void printValue(int value) {
+            if(value==1){
+                System.out.println(value);
+            }else {
+                printValue(value-1);
+                System.out.println(value);
+            }
     }
 }
--- Homework4/src/Task6.java
+++ Homework4/src/Task6.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Task6 {
+    public static void main(String[] args) {
+        int h = 9, w = 6;
+
+        drawRectangle(h, w);
+    }
+
+    /**
+     * draws rectangle with width W and heights H
+     */
+    private static void drawRectangle(int h, int w) {
+        char filling = 134;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                System.out.print(filling);
+            }
+            System.out.println();
+        }
+    }
 }
