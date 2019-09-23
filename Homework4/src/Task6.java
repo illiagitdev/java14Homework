8c2d141efd35a343f07148ab17933b6d82a64acc
illiagitdev <illya.posv.b@gmail.com>
2019-09-23 10:47:39 +0300

    Homework4.task6: task 6 - logic

--- Homework4/src/Task6.java
+++ Homework4/src/Task6.java
@@ -10,15 +10,15 @@ public class Task6 {
      * draws rectangle with width W and heights H
      */
     private static void drawRectangle(int h, int w) {//todo:add second recursion
-        char filling = 43;
-        int q=0;
-            if (w >0) {
-                System.out.println();
-                drawRectangle(h, w - 1);
-            } else {
-                System.out.print(filling);
-            }
-        drawRectangle(--h,w);
+//        char filling = 43;
+//        int q=0;
+//            if (w >0) {
+//                System.out.println();
+//                drawRectangle(h, w - 1);
+//            } else {
+//                System.out.print(filling);
+//            }
+//        drawRectangle(--h,w);
         //System.out.println(w==0?"\n":drawRectangle(h,w-1));
 
 //        for (int i = 0; i < h; i++) {
