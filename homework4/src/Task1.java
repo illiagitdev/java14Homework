66c91ad858f941a91028029c95b4aec367ae0f0f
illiagitdev <illya.posv.b@gmail.com>
2019-09-23 13:55:59 +0300

    recreated project

---
+++ homework4/src/Task1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.print("Введіть ціле число: ");
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+
+        for (int i = 1; i <= value; i++) {
+            System.out.println(i);
+        }
+    }
+}
