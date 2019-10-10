8c3d3ea9becc7d402f39d98dd00153c6fe4de929
illiagitdev <illya.posv.b@gmail.com>
2019-10-11 01:05:48 +0300

    OnlineShop: show method in parent class

--- OnlineStore/src/categories/Goods.java
+++ OnlineStore/src/categories/Goods.java
@@ -7,7 +7,21 @@ public class Goods {
     private String description;
     private double price;
 
-    public void show(){
+    public Goods(int id, String name, int amount, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.description = description;
+        this.price = price;
+    }
 
+    public void show(){
+        System.out.println("Goods{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}');
     }
 }
