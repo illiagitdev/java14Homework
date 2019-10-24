f05374042f5a8f46d0d359a2947407766177cc26
illiagitdev <illya.posv.b@gmail.com>
2019-10-24 16:30:29 +0300

    OnlineShop: refractor

--- OnlineStore/src/store/functionaliti/Utils.java
+++ OnlineStore/src/store/functionaliti/Utils.java
@@ -1,21 +1,16 @@
 package store.functionaliti;
 
+
 import java.util.Scanner;
 
 public class Utils {
-    public static void readFile(String path){
-
-    }
-
     public static int getInt() {
         Scanner scan=new Scanner(System.in);
-        int in=scan.hasNextInt()?scan.nextInt():getInt();
-        return in;
+        return   scan.hasNextInt()?scan.nextInt():getInt();
     }
 
     public static String getStr() {
         Scanner scan=new Scanner(System.in);
-        String in=scan.next();
-        return in;
+        return scan.next();
     }
 }
