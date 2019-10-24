78ed254c0887426e519b07ee16ff84544382f065
illiagitdev <illya.posv.b@gmail.com>
2019-10-24 16:30:05 +0300

    OnlineShop: added setter

--- OnlineStore/src/goods/components/tech/OperatingSystem.java
+++ OnlineStore/src/goods/components/tech/OperatingSystem.java
@@ -3,6 +3,10 @@ package goods.components.tech;
 public class OperatingSystem implements TechSpecifics {
     private String operatingSystem;
 
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
     public String getOperatingSystem() {
         return operatingSystem;
     }
