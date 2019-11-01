6c9adde0a30f44a605f18ff634c5bdcc38c457fa
illiagitdev <illya.posv.b@gmail.com>
2019-11-01 15:22:30 +0200

    Company: added getters and setters

--- OOP_Polimorphism06/src/company/employee/employeetype/Frilance.java
+++ OOP_Polimorphism06/src/company/employee/employeetype/Frilance.java
@@ -6,17 +6,37 @@ public class Frilance extends Employee {
     private float workHours;
     private float hourPrice;
 
+    public float getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(float workHours) {
+        if (workHours >= 0) {
+            this.workHours = workHours;
+        } else this.workHours = 0;
+    }
+
+    public float getHourPrice() {
+        return hourPrice;
+    }
+
+    public void setHourPrice(float hourPrice) {
+        if (hourPrice >= 0) {
+            this.hourPrice = hourPrice;
+        } else this.hourPrice = 0;
+    }
+
     public Frilance(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
     @Override
     public float salaryCalc() {
-        return hourPrice*workHours;
+        return hourPrice * workHours;
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", salary" + salaryCalc() + '$';
+        return super.toString() + ", salary = " + salaryCalc() + " $";
     }
 }
--- OOP_Polimorphism06/src/company/employee/employeetype/NonFixSalary.java
+++ OOP_Polimorphism06/src/company/employee/employeetype/NonFixSalary.java
@@ -7,6 +7,36 @@ public class NonFixSalary extends Employee {
     private float workDays;
     private float hourlyPay;
 
+    public float getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(float workHours) {
+        if (workHours >= 0) {
+            this.workHours = workHours;
+        } else this.workHours = 0;
+    }
+
+    public float getWorkDays() {
+        return workDays;
+    }
+
+    public void setWorkDays(float workDays) {
+        if (workDays >= 0) {
+            this.workDays = workDays;
+        } else this.workDays = 0;
+    }
+
+    public float getHourlyPay() {
+        return hourlyPay;
+    }
+
+    public void setHourlyPay(float hourlyPay) {
+        if (hourlyPay >= 0) {
+            this.hourlyPay = hourlyPay;
+        } else this.hourlyPay = 0;
+    }
+
     public NonFixSalary(String firstName, String lastName) {
         super(firstName, lastName);
     }
@@ -18,6 +48,6 @@ public class NonFixSalary extends Employee {
 
     @Override
     public String toString() {
-        return super.toString() + ", salary" + salaryCalc() + '$';
+        return super.toString() + ", salary = " + salaryCalc() + " $";
     }
 }
