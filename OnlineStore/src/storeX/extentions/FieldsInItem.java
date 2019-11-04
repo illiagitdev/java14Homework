22cef1997809423c61864c6743f3ba461ca1d1d7
illiagitdev <illya.posv.b@gmail.com>
2019-11-04 17:38:29 +0200

    OnlineShop*: enum for indexing saved files

---
+++ OnlineStore/src/storeX/extentions/FieldsInItem.java
@@ -0,0 +1,11 @@
+package store.functionaliti;
+
+public enum FieldsInItem {
+    PHONE(15),COMPUTER(8),VEGETABLES(4),SWEETS(4),FRESHWATER(5),SWEETWATER(4),CLOTHES(6);
+
+    public final int index;
+
+    FieldsInItem(int index) {
+        this.index=index;
+    }
+}
