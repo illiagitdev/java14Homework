a0fe4119f79c10f7c5cf9d342f045aca614ee0fe
illiagitdev <illya.posv.b@gmail.com>
2019-11-05 17:30:23 +0200

    OnlineShop*: computer 16 fields

--- OnlineStore/src/storeX/extentions/FieldsInItem.java
+++ OnlineStore/src/storeX/extentions/FieldsInItem.java
@@ -1,7 +1,7 @@
 package storeX.extentions;
 
 public enum FieldsInItem {
-    PHONE(15),COMPUTER(8),VEGETABLES(4),SWEETS(4),FRESHWATER(5),SWEETWATER(4),CLOTHES(6);
+    PHONE(15),COMPUTER(16),VEGETABLES(4),SWEETS(4),FRESHWATER(5),SWEETWATER(4),CLOTHES(6);
 
     public final int index;
 
