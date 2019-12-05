5396a68290bee4d2667ef72d44de2e5f61ea08b2
illiagitdev <illya.posv.b@gmail.com>
2019-12-05 19:16:42 +0200

    core*

--- Core_Alex/src/Main.java
+++ Core_Alex/src/Main.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Runnable runnable=new RunClass();
+
+        Thread thread = new Thread("not main");
+        thread.start();
+
+        Thread thread1=new Thread(runnable);
+        thread1.start();
+
+        threadInfo(thread);
+        threadInfo(thread1);
+        threadInfo(Thread.currentThread());
+    }
+
+    public static void threadInfo(Thread thread) {
+        System.out.println(thread.getName());
+        System.out.println(thread.getThreadGroup().getName());
+    }
+
+
 }
--- Core_Alex/src/RunClass.java
+++ Core_Alex/src/RunClass.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
-public class RunClass {
+public class RunClass implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "runnable from class");
+    }
 }
