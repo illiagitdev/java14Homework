67798a965da90b5fb1e442d82da5bbba34483812
illiagitdev <illya.posv.b@gmail.com>
2019-12-09 07:57:08 +0200

    Threading: utilits

--- MultithreadingLambda/src/services/Utylits.java
+++ MultithreadingLambda/src/services/Utylits.java
@@ -1,20 +1,28 @@
-package task1;
+package services;
 
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Utylits {
-    public static int getInt(){
-        Scanner input=new Scanner(System.in);
-        return input.hasNextInt()?input.nextInt():getInt();
+    public static int getInt() {
+        Scanner input = new Scanner(System.in);
+        return input.hasNextInt() ? input.nextInt() : getInt();
     }
 
-    public void randomWaitTime(int seconds) {
+    public static void randomWaitTime(int seconds) {
         Random random = new Random();
         int result = random.nextInt(seconds) + 1;
         try {
             TimeUnit.SECONDS.sleep(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void waitTime(double passDor) {
+        try {
+            TimeUnit.MILLISECONDS.sleep((long) (passDor * 1000));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
