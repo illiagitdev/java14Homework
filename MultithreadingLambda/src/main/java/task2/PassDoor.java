fbab6c61d46b7661d879aaee16d745a81fe3e5f1
illiagitdev <illya.posv.b@gmail.com>
2019-12-15 10:36:51 +0200

    pass and leave with synchronized by instance of class

--- MultithreadingLambda/src/main/java/task2/PassDoor.java
+++ MultithreadingLambda/src/main/java/task2/PassDoor.java
@@ -3,19 +3,21 @@ package main.java.task2;
 import main.java.services.Utylits;
 
 public class PassDoor {
-    public static void enterLibrary(double passDor) {
-        System.out.printf("Відвідувач підійшов до дверей ззовні. Відвідувач № %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-        System.out.printf("Відвідувач проходить крізь двері всередину. Відвідувач № %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-        Utylits.waitTime(passDor);
-        System.out.printf("Відвідувач пройшов крізо двері всередину. Відвідувач № %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+    public void enterLibrary(double passDor) {
+        synchronized (this) {
+            System.out.printf("Відвідувач підійшов до дверей ззовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            Utylits.waitTime(passDor);
+            System.out.printf("Відвідувач пройшов крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+        }
     }
 
-    public static void exitLibrary(double passDor) {
-        System.out.printf("Відвідувач підійшов до дверей зсередини. Відвідувач № %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-        Lock
-        System.out.printf("Відвідувач проходить крізь двері назовні. Відвідувач № %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-        Utylits.waitTime(passDor);
-        System.out.printf("Відвідувач пройшов крізо двері назовні. Відвідувач № %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+    public void exitLibrary(double passDor) {
+        synchronized (this) {
+            System.out.printf("Відвідувач підійшов до дверей зсередини. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            Utylits.waitTime(passDor);
+            System.out.printf("Відвідувач пройшов крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+       }
     }
-
 }
