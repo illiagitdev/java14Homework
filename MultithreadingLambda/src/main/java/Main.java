8a3371f920bdd17b2990cfd1823bf515fc340532
illiagitdev <illya.posv.b@gmail.com>
2019-12-16 17:57:26 +0200

    separate main for both tasks

--- MultithreadingLambda/src/main/java/Main.java
+++ MultithreadingLambda/src/main/java/Main.java
@@ -5,12 +5,8 @@ import main.java.task2.LibraryDoor;
 
 public class Main {
     public static void main(String[] args) {
-        //перша частина завдання
-//        Library library1 = Library.getInstance();
-//        library1.run();
-
         //друга частина завдання з дверима
-        LibraryDoor library2 = LibraryDoor.getInstance();
-        library2.run();
+//        LibraryDoor library2 = LibraryDoor.getInstance();
+//        library2.run();
     }
 }
---
+++ MultithreadingLambda/src/main/java/task1/Main.java
@@ -0,0 +1,8 @@
+package main.java.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Library library = Library.getInstance();
+        library.run();
+    }
+}
