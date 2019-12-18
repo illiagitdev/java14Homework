4738a9430d646ef137f4c4d4fc4c7a0e054b494a
illiagitdev <illya.posv.b@gmail.com>
2019-12-19 00:29:55 +0200

    synchronized(this) added to methons

--- MultithreadingLambda/src/main/java/task2/Door.java
+++ MultithreadingLambda/src/main/java/task2/Door.java
@@ -4,17 +4,20 @@ import main.java.services.Utylits;
 
 public class Door {
     public void enterLibrary(double passDoorTime) {
+        synchronized (this) {
             System.out.printf("Відвідувач підійшов до дверей ззовні. %s\n", Thread.currentThread().getName());
             System.out.printf("Відвідувач проходить крізь двері всередину. %s\n", Thread.currentThread().getName());
             Utylits.waitTime(passDoorTime);
             System.out.printf("Відвідувач пройшов крізь двері всередину. %s\n", Thread.currentThread().getName());
+        }
     }
 
     public void exitLibrary(double passDoorTime) {
+        synchronized (this) {
             System.out.printf("Відвідувач підійшов до дверей зсередини. %s\n", Thread.currentThread().getName());
             System.out.printf("Відвідувач проходить крізь двері назовні. %s\n", Thread.currentThread().getName());
             Utylits.waitTime(passDoorTime);
             System.out.printf("Відвідувач пройшов крізь двері назовні. %s\n", Thread.currentThread().getName());
+        }
     }
-
 }
