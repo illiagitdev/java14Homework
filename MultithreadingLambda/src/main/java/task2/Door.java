34a142c9e8e7661493e79873fc9cce55543f82fd
illiagitdev <illya.posv.b@gmail.com>
2019-12-18 18:00:31 +0200

    deleted ID from sout

--- MultithreadingLambda/src/main/java/task2/Door.java
+++ MultithreadingLambda/src/main/java/task2/Door.java
@@ -4,17 +4,17 @@ import main.java.services.Utylits;
 
 public class Door {
     public void enterLibrary(double passDoorTime) {
-            System.out.printf("Відвідувач підійшов до дверей ззовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-            System.out.printf("Відвідувач проходить крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач підійшов до дверей ззовні. %s\n", Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері всередину. %s\n", Thread.currentThread().getName());
             Utylits.waitTime(passDoorTime);
-            System.out.printf("Відвідувач пройшов крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач пройшов крізь двері всередину. %s\n", Thread.currentThread().getName());
     }
 
     public void exitLibrary(double passDoorTime) {
-            System.out.printf("Відвідувач підійшов до дверей зсередини. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-            System.out.printf("Відвідувач проходить крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач підійшов до дверей зсередини. %s\n", Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері назовні. %s\n", Thread.currentThread().getName());
             Utylits.waitTime(passDoorTime);
-            System.out.printf("Відвідувач пройшов крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач пройшов крізь двері назовні. %s\n", Thread.currentThread().getName());
     }
 
 }
--- MultithreadingLambda/src/main/java/task2/PassDoor.java
+++ MultithreadingLambda/src/main/java/task2/PassDoor.java
@@ -5,19 +5,19 @@ import main.java.services.Utylits;
 public class PassDoor {
     public void enterLibrary(double passDor) {
         synchronized (this) {
-            System.out.printf("Відвідувач підійшов до дверей ззовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-            System.out.printf("Відвідувач проходить крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач підійшов до дверей ззовні.%s\n", Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері всередину. %s\n", Thread.currentThread().getName());
             Utylits.waitTime(passDor);
-            System.out.printf("Відвідувач пройшов крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач пройшов крізь двері всередини. %s\n", Thread.currentThread().getName());
         }
     }
 
     public void exitLibrary(double passDor) {
         synchronized (this) {
-            System.out.printf("Відвідувач підійшов до дверей зсередини. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
-            System.out.printf("Відвідувач проходить крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач підійшов до дверей зсередини. %s\n", Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері назовні. %s\n", Thread.currentThread().getName());
             Utylits.waitTime(passDor);
-            System.out.printf("Відвідувач пройшов крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач пройшов крізь двері назовні. %s\n", Thread.currentThread().getName());
        }
     }
 }
