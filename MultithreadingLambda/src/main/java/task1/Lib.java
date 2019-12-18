20895205827f4a577e3301358c67cda2610035ac
illiagitdev <illya.posv.b@gmail.com>
2019-12-19 00:28:59 +0200

    added door to the library

--- MultithreadingLambda/src/main/java/task1/Lib.java
+++ MultithreadingLambda/src/main/java/task1/Lib.java
@@ -1,6 +1,7 @@
 package main.java.task1;
 
 import main.java.services.Utylits;
+import main.java.task2.Door;
 
 import java.util.concurrent.Semaphore;
 
@@ -12,15 +13,18 @@ public class Lib {
         System.out.print("Визначте скфльки людей хоче відвідати бібліотеку бібліотеці: ");
         int visitorsAmount = Utylits.getPositiveInt();
         long startPoint = System.currentTimeMillis();
+        Door door = new Door();
         for (int i = 0; i < visitorsAmount; i++) {
             new Thread(() -> {
                 String threadName = Thread.currentThread().getName();
                 System.out.printf("Прийшов до входу в бібліотеку. %s %s\n", threadName, System.currentTimeMillis() - startPoint);
                 try {
                     semaphore.acquire();
+                    door.enterLibrary(0.5);
                     System.out.printf("Зайшов у бібліотеку. %s %s\n", threadName, System.currentTimeMillis() - startPoint);
                     int reads = Utylits.randomWaitTime(5);
                     System.out.printf("Читає книгу (%s ms). %s \n", reads, threadName);
+                    door.exitLibrary(0.5);
                     System.out.printf("Відвідувач вийшов з бібліотеки. %s %s\n", threadName, System.currentTimeMillis() - startPoint);
                     semaphore.release();
                 } catch (InterruptedException e) {
