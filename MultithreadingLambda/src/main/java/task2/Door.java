a153b227377e11d053e1e8704d390808160ebe65
illiagitdev <illya.posv.b@gmail.com>
2019-12-18 02:46:16 +0200

    https://www.baeldung.com/java-cyclic-barrier - check it

--- MultithreadingLambda/src/main/java/task2/Door.java
+++ MultithreadingLambda/src/main/java/task2/Door.java
@@ -1,4 +1,20 @@
 package main.java.task2;
 
+import main.java.services.Utylits;
+
 public class Door {
+    public void enterLibrary(double passDoorTime) {
+            System.out.printf("Відвідувач підійшов до дверей ззовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            Utylits.waitTime(passDoorTime);
+            System.out.printf("Відвідувач пройшов крізь двері всередину. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+    }
+
+    public void exitLibrary(double passDoorTime) {
+            System.out.printf("Відвідувач підійшов до дверей зсередини. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            System.out.printf("Відвідувач проходить крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+            Utylits.waitTime(passDoorTime);
+            System.out.printf("Відвідувач пройшов крізь двері назовні. %d %s\n", Thread.currentThread().getId(), Thread.currentThread().getName());
+    }
+
 }
