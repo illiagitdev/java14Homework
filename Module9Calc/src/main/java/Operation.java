f9d6758ddc192dd19a5b53f94e4339a1b27ab0c3
illiagitdev <illya.posv.b@gmail.com>
2019-12-19 15:49:46 +0200

    Added future, calculate in thread, returns value to main thread

--- Module9Calc/src/main/java/Operation.java
+++ Module9Calc/src/main/java/Operation.java
@@ -1,66 +1,54 @@
 package main.java;
 
 import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 public class Operation {
 
-    public void getInput() {
+    public void run() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
         String[] signs = {"+", "-", "*", "/", "%", "==", ">", "<"};
-        while (true){double val1 = getNumber();
-        double val2 = getNumber();
-        String sigh = getSigh(signs);
-        System.out.println("Main thread " + Thread.currentThread().getName());
-        new Thread(() -> {
-            evaluate(val1, val2, sigh);
-        }).start();}
+        Callable<Object> task = () -> evaluate(getNumber(), getNumber(), getSigh(signs));
+
+        Future future = executor.submit(task);
+        try {
+            if (!future.isDone()){
+                executor.shutdownNow();
+            }
+            System.out.println(future.get() + " {returns in " + Thread.currentThread().getName() + '}');
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void evaluate(double val1, double val2, String sigh) {
+    private Object evaluate(double val1, double val2, String sigh) {
+        String result = " {calculate in " + Thread.currentThread().getName() + '}';
         switch (sigh) {
-            case "+": {
-                        System.out.println("a + b = " + (val1 + val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            case "-": {
-                        System.out.println("a - b = " + (val1 - val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            case "*": {
-                        System.out.println("a * b = " + (val1 * val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            case "/": {
-                        System.out.println("a / b = " + (val1 / val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            case "%": {
-                        System.out.println("a % b = " + (val1 % val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            case "==": {
-                        System.out.println("a == b = " + (val1 == val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            case ">": {
-                        System.out.println("a > b = " + (val1 > val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            case "<": {
-                System.out.println("a < b = " + (val1 < val2) + "  " + Thread.currentThread().getName());
-                break;
-            }
-            default: {
-                System.out.println("No such operation!" + "  " + Thread.currentThread().getName());
-                break;
-            }
+            case "+":
+                return "a + b = " + (val1 + val2) + result;
+            case "-":
+                return "a - b = " + (val1 - val2) + result;
+            case "*":
+                return "a * b = " + (val1 * val2) + result;
+            case "/":
+                return "a / b = " + (val1 / val2) + result;
+            case "%":
+                return "a % b = " + (val1 % val2) + result;
+            case "==":
+                return "a == b = " + (val1 == val2) + result;
+            case ">":
+                return "a > b = " + (val1 > val2) + result;
+            case "<":
+                return "a < b = " + (val1 < val2) + result;
         }
+        return -1;
     }
 
     private String getSigh(String[] signs) {
         System.out.println("Виберіть операцію:");
         Scanner input = new Scanner(System.in);
-        for (int i = 0; i < signs.length; i++)
-            System.out.print((i + 1)+"  " + signs[i]+",\t");
+        IntStream.range(0, signs.length).mapToObj(i -> (i + 1) + ":  " + signs[i] + ",\t").forEach(System.out::print);
         return input.hasNext() ? input.nextLine() : getSigh(signs);
     }
 
